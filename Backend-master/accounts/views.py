2bba411ad1dadfdd0c629096cd11a4705aed5a9e
hasnaaha2 <hasnaaahamed2@gmail.com>
2021-02-26 17:05:33 +0200

    update

--- Backend-master/accounts/views.py
+++ Backend-master/accounts/views.py
@@ -1,9 +1,43 @@
+
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.core.paginator import Paginator
 from django.shortcuts import redirect,render
 from .forms import SignupForm, UserForm, ProfileForm
 from django.contrib.auth import authenticate,login
+from django.contrib.auth.models import User
+from django.template.loader import render_to_string
+from django.conf import settings
 from .models import Profile
 from django.urls import reverse
+from django.http import HttpResponseRedirect
+from tracksapp.models import courses
 # Create your views here.
+@ login_required
+def favourite_list(request):
+    profile = Profile.objects.get(user=request.user)
+    new = courses.newmanager.filter(favourites=request.user)
+    paginator = Paginator(new, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request,'accounts/favourites.html',{'profile':profile, 'new':page_obj})
+
+
+
+@ login_required
+def favourite_add(request, id):
+    course=get_object_or_404(courses,id=id)
+    if course.favourites.filter(id=request.user.id).exists():
+        course.favourites.remove(request.user)
+    else:
+        course.favourites.add(request.user) 
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])     
+
+
 
 
 def signup(request):
@@ -23,9 +57,10 @@ def signup(request):
 
 
 
+
 def profile(request):
    profile = Profile.objects.get(user=request.user)
-   return render(request,'accounts/profile.html',{'profile':profile})
+   return render(request,'accounts/profile.html',{'profile':profile,})
 
 
 def profile_edit(request):
